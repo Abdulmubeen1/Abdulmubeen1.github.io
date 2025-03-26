fc87cf87ff555d72c00bd0df18972e9c52b26062
amuneen <amubeen@ualr.edu>
2025-03-26 02:21:42 -0500

    code chang

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navLinks = document.querySelector('.nav-links');
-
-hamburgerMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
